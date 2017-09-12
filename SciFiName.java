0d8e70249042213d7233030533e183bd2d47e319
Jaiveer Singh <singh9999@mydusd.org>
2017-09-12 10:33:11 -0700

    Finished 1_1_5

--- SciFiName.java
+++ SciFiName.java
@@ -23,20 +23,30 @@ public class SciFiName
 
         // notice this methd (print) does not print a newline
         System.out.print("Enter your first name: ");
-        String firstName = UserInput.getString();
+        String firstName = UserInput.getString().toLowerCase();
         System.out.print("Enter your last name: ");
-        String lastName = UserInput.getString();
+        String lastName = UserInput.getString().toLowerCase();
         System.out.print("Enter the city where you or one of your parents were born: ");
-        String city = UserInput.getString();
+        String city = UserInput.getString().toLowerCase();
         System.out.print("Enter the name of your grammar school: ");
-        String school = UserInput.getString();
+        String school = UserInput.getString().toLowerCase();
         System.out.print("Enter the first name of a sibling or other relative: ");
-        String relativeName1 = UserInput.getString();
+        String relativeName1 = UserInput.getString().toLowerCase();
         System.out.print("Enter the first name of a second sibling or relative: ");
-        String relativeName2 = UserInput.getString();
+        String relativeName2 = UserInput.getString().toLowerCase();
 
         // generate a sciFi name
-   
+        String sciFiFirst = firstName.substring(0, 3) + lastName.substring(0, 2);
+        String sciFiLast = city.substring(0, 2) + school.substring(0, 3);
+        String sciFiOrigin = relativeName1.substring((int) (Math.random() * (relativeName1.length() - 1)) + 1)
+        + relativeName2.substring((int) (Math.random() * (relativeName2.length() - 1)) + 1);
+        sciFiFirst = sciFiFirst.substring(0,1).toUpperCase() + sciFiFirst.substring(1);;
+        sciFiLast = sciFiLast.substring(0,1).toUpperCase() + sciFiLast.substring(1);;
+        sciFiOrigin = sciFiOrigin.substring(0,1).toUpperCase() + sciFiOrigin.substring(1);;
+
+        
+        System.out.println("Hello " + sciFiFirst + " " + sciFiLast + " of " + sciFiOrigin + ". Welcome!");
+        
         
     }
 }
